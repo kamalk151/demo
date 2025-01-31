b1d9a823274cd3a19392f5ab670c56adb7195dc7
kamalk151 <kamalk151@gmail.com>
2025-01-31 12:41:00 +0530

    initial setup

---
+++ src/Header.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function Header() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>WhatsApp</h1>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
